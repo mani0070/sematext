ab08e03115da30422db5207e70c043ee9e2e0aba
Manimaran Chandrasekaran <mani@dinventive.com>
2017-01-09 20:33:51 +0000

    Update spmclient.sh

--- spmclient.sh
+++ spmclient.sh
@@ -5,7 +5,7 @@ add-apt-repository "deb http://pub-repo.sematext.com/debian sematext main"
 wget -O- https://pub-repo.sematext.com/ubuntu/sematext.gpg.key | apt-key add -
 
 echo "${serverip} ${serverurl}" > /etc/hosts
-apt-get update
+apt-get update -y
 # install mono
 apt-get install mono-complete -y 
 apt-get install default-jdk -y
